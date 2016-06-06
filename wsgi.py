493f5b123c0923cf1968c71cf026188df6e33038
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-06-06 15:44:47 +1000

    Rename app to application.

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
