7e7b99d15d9b90677d4438e6e80a9af72cff25d4
gokamoto-publicrepo <34248015+gokamoto-publicrepo@users.noreply.github.com>
2017-12-06 11:52:16 -0800

    Update wsgi.py
    adding a countdown string and testing if the system catches code errors

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import time
+from datetime import date
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "C2C24 Demo Python App"
+    today = date.today()
+    april1 = date(2018, 4, 1)
+    countdownstring = "C2C24 Demo Python App "
+    if april1 > today:
+        time_to_april1 = abs(april1 - today)
+        days_to_april1 = time_to_april1.days
+        countdownstring += "Days to April 1: "
+        countdownstring.append(days_to_april1)
+    return countdownstring
 
 if __name__ == "__main__":
     application.run()
