5e0617ccf6cc348e2aa48c4a3185e4786a551017
gokamoto-publicrepo <34248015+gokamoto-publicrepo@users.noreply.github.com>
2017-12-06 12:06:06 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,13 +6,14 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     today = date.today()
-    april1 = date(2018, 4, 1)
+    #april1 = date(2018, 4, 1)
     countdownstring = "C2C24 Demo Python App "
-    if april1 > today:
-        time_to_april1 = abs(april1 - today)
-        days_to_april1 = time_to_april1.days
-        countdownstring += "Days to April 1: "
-        countdownstring.append(days_to_april1)
+    countdownstring += "Days to April 1"
+    #if april1 > today:
+        #time_to_april1 = abs(april1 - today)
+        #days_to_april1 = time_to_april1.days
+        #countdownstring += " Days to April 1: "
+        #countdownstring.append(days_to_april1)
     return countdownstring
 
 if __name__ == "__main__":
