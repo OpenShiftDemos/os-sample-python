2e03785032c19cb0de679ff4408910a6e64ffd51
MMC0DE <43372424+mauricemensing@users.noreply.github.com>
2018-09-20 14:00:42 +0200

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-application = Flask(__name__)
-
-@application.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
-
-if __name__ == "__main__":
-    application.run()
