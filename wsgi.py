939508c60644103a2b1e780d094bbe01c698a168
Matthew Callaway <mcallawa@wustl.edu>
2018-11-13 17:12:18 -0600

    Add liveness and readiness checks.

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,23 @@
+
+import os,sys
+from stat import *
 from flask import Flask
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+@application.route("/alive")
+def alive():
+    return "Alive"
+
+@application.route("/ready")
+def ready():
+    mode = os.stat("/").st_mode
+    msg = "Ready: %s" % mode
+    return msg
 
 if __name__ == "__main__":
     application.run()
