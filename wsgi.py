f1b6047edc1fd6c330d03df21ce941f260e7958d
muath22 <40863580+muath22@users.noreply.github.com>
2020-04-05 02:05:11 +0300

    JKEB-7 changed to Aramco

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/hello")
 def hello():
-    return "Hello world!"
+    return "Hello Aramco!"
 
 if __name__ == "__main__":
     application.run()
