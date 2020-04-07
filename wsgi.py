7b4b92150298c57c79f3315e7882a9ac78bcf282
muath22 <40863580+muath22@users.noreply.github.com>
2020-04-07 20:32:53 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,32 +1,9 @@
-import os
-
 from flask import Flask
+application = Flask(__name__)
 
+@application.route("/hello")
+def hello():
+    return "Hello A!"
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
+if __name__ == "__main__":
+    application.run()
