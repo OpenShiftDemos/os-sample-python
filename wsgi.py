ba38370e821e934eab333d6755aa3d455fad97c0
muath22 <40863580+muath22@users.noreply.github.com>
2020-04-07 05:08:35 +0300

    JKEB-12
    JKEB-12

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/hello")
 def hello():
-    return "Hello New Branch!  https://www.worldometers.info/coronavirus/"
+    return "Hello New Branch!  COVID 19 https://www.worldometers.info/coronavirus/"
 
 if __name__ == "__main__":
     application.run()
