259d9caea98efe125a3429320536390b0b74d9fc
muath22 <40863580+muath22@users.noreply.github.com>
2020-04-08 04:27:34 +0300

    JKEB-11 test 2 bug
    JKEB-11 test 2 bug

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/hello")
 def hello():
-    return "Hello Final!"
+    return "Hello JKEB-11 test 2 bug!"
 
 if __name__ == "__main__":
     application.run()
