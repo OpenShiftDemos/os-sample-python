dc237cbb629d77793f36a85c1f7805bd10a6dbac
muath22 <40863580+muath22@users.noreply.github.com>
2020-04-09 09:38:48 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/hello")
 def hello():
-    return "I love Khalid V2!"
+    return "I love Khalid v3!"
 
 if __name__ == "__main__":
     application.run()
